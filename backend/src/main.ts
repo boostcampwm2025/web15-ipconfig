28b2145e09a90d3dbd8d98a0bfd0c44b66f0d017
davidpro08 <davidmoon0817@gmail.com>
2025-12-15 18:32:20 +0900

    chore: swagger 세팅

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const configSwagger = new DocumentBuilder()
+    .setTitle('BangBang API')
+    .setDescription('BangBang API description')
+    .setVersion('1.0')
+    .addTag('BangBang')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
