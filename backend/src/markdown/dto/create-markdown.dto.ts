73f0fc10ae253db1672930e7820751ee890055d1
davidpro08 <davidmoon0817@gmail.com>
2025-12-17 23:58:20 +0900

    feat: 임시 데이터로 마크다운 만들기
    - 임시 데이터를 넣어서 마크다운을 만들도록 해놨습니다. 데이터 형식을 정한 다음에 세부 사항을 만드는 게 좋을 것 같습니다.

--- backend/src/markdown/dto/create-markdown.dto.ts
+++ backend/src/markdown/dto/create-markdown.dto.ts
@@ -1,1 +1,34 @@
-export class CreateMarkdownDto {}
+import { IsArray, IsString } from 'class-validator';
+
+// 임시 팀 스타일
+export class TeamStyle {
+  name: string;
+  score: number;
+}
+
+// 임시 기술 스택
+export class TechStack {
+  name: string;
+  version: string;
+  status: string;
+}
+
+// 임시 그라운드 룰
+export class GroundRules {
+  name: string;
+  description: string;
+}
+
+export class CreateMarkdownDto {
+  @IsArray()
+  teamStyles: TeamStyle[];
+
+  @IsArray()
+  techStacks: TechStack[];
+
+  @IsArray()
+  groundRules: GroundRules[];
+
+  @IsString()
+  workspaceName: string;
+}
