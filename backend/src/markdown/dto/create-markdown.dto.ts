11b6d00734efd74398e39ebe1abde7213c01240e
davidpro08 <davidmoon0817@gmail.com>
2025-12-18 00:29:04 +0900

    feat: swagger를 위한 ApiProperty 추가
    - 일단 임시 데이터긴 하지만, 테스트를 위해 ApiProperty를 추가했습니다.

--- backend/src/markdown/dto/create-markdown.dto.ts
+++ backend/src/markdown/dto/create-markdown.dto.ts
@@ -1,35 +1,50 @@
 import { IsArray, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 // 임시 팀 스타일
 export class TeamStyle {
+  @ApiProperty({ description: '팀 스타일 이름', example: '작업 시간' })
   name: string;
+
+  @ApiProperty({ description: '팀 스타일 점수', example: 100 })
   score: number;
 }
 
 // 임시 기술 스택
 export class TechStack {
+  @ApiProperty({ description: '기술 스타일 이름', example: 'React' })
   name: string;
+
+  @ApiProperty({ description: '기술 스타일 버전', example: '18.2.0' })
   version: string;
   // 선택된 거만 기록하기 때문에 이건 필요 없지 않을까?
+  @ApiProperty({ description: '기술 스타일 상태', example: '사용' })
   status: string;
 }
 
 // 임시 그라운드 룰
 export class GroundRules {
+  @ApiProperty({ description: '그라운드 룰 이름', example: '네이밍 컨벤션' })
   name: string;
+
+  @ApiProperty({ description: '그라운드 룰 설명', example: 'camelCase' })
   description: string;
 }
 
 export class CreateMarkdownDto {
+  @ApiProperty({ description: '팀 스타일', type: [TeamStyle] })
   @IsArray()
   teamStyles: TeamStyle[];
 
+  @ApiProperty({ description: '기술 스타일', type: [TechStack] })
   @IsArray()
   techStacks: TechStack[];
 
+  @ApiProperty({ description: '그라운드 룰', type: [GroundRules] })
   @IsArray()
   groundRules: GroundRules[];
 
+  @ApiProperty({ description: '워크스페이스 이름', example: 'test' })
   @IsString()
   workspaceName: string;
 }
--- backend/src/markdown/markdown.controller.ts
+++ backend/src/markdown/markdown.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiCreatedResponse, ApiResponse } from '@nestjs/swagger';
 import { MarkdownService } from './markdown.service';
 import { CreateMarkdownDto } from './dto/create-markdown.dto';
 
@@ -6,7 +7,21 @@ import { CreateMarkdownDto } from './dto/create-markdown.dto';
 export class MarkdownController {
   constructor(private readonly markdownService: MarkdownService) {}
 
+  // TODO: 현재는 POST로 되어 있는데, 나중에 GET 요청으로 바꾸고, 위젯을 읽어와서 DTO를 만들고 createMarkdown 메서드를 호출하도록 수정해야 합니다.
   @Post()
+  @ApiOperation({ summary: '마크다운 생성' })
+  @ApiCreatedResponse({
+    description: '마크다운 생성 성공',
+    type: String,
+  })
+  @ApiResponse({
+    status: 400,
+    description: '잘못된 요청 (예: DTO validation 실패)',
+  })
+  @ApiResponse({
+    status: 500,
+    description: '서버 내부 오류',
+  })
   createMarkdown(@Body() createMarkdownDto: CreateMarkdownDto) {
     return this.markdownService.createMarkdown(createMarkdownDto);
   }
