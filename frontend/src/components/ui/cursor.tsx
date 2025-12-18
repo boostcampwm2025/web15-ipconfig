db9ddf3990b1643227c066263c23d94f6f04f6a9
davidpro08 <davidmoon0817@gmail.com>
2025-12-19 00:26:01 +0900

    refactor: 바탕색 넣고 크기 키우기

--- frontend/src/components/ui/cursor.tsx
+++ frontend/src/components/ui/cursor.tsx
@@ -14,15 +14,17 @@ function Cursor({ nickname, color, backgroundColor, x, y }: CursorProps) {
   return (
     <div className="pointer-events-none -translate-x-1 -translate-y-1 select-none">
       <div className="flex flex-col items-start">
-        <LuMousePointer2
-          className="drop-shadow-[0_1px_2px_rgba(0,0,0,0.25)]"
-          style={{
-            color: renderColor,
-            background: backgroundColor,
-            x: x,
-            y: y,
-          }}
-        />
+        <div>
+          <LuMousePointer2
+            className="size-6 drop-shadow-[0_1px_2px_rgba(0,0,0,0.25)]"
+            style={{
+              color: renderColor,
+              fill: backgroundColor,
+              x: x,
+              y: y,
+            }}
+          />
+        </div>
         {nickname}
       </div>
     </div>
