9966039a0fa635b04ae9151bd9771730be5be4ea
davidpro08 <davidmoon0817@gmail.com>
2026-01-06 14:51:15 +0900

    feat: 위젯 타입으로 위젯을 찾도록 메서드 추가
    - 나중에 마크다운에서 위젯 타입으로 쉽게 위젯을 찾기 위해 메서드를 추가했습니다.

--- backend/src/widget/widget.memory.service.ts
+++ backend/src/widget/widget.memory.service.ts
@@ -3,6 +3,7 @@ import { IWidgetService } from './widget.interface';
 import { CreateWidgetDto } from './dto/create-widget.dto';
 import { UpdateWidgetDto } from './dto/update-widget.dto';
 import { UpdateWidgetLayoutDto } from './dto/update-widget-layout.dto';
+import { WidgetType } from './dto/widget-content.dto';
 
 @Injectable()
 export class WidgetMemoryService implements IWidgetService {
@@ -38,6 +39,20 @@ export class WidgetMemoryService implements IWidgetService {
     const widget = widgets.get(widgetId);
     if (!widget) {
       throw new NotFoundException(`Widget with ID ${widgetId} not found`);
+    }
+    return Promise.resolve(widget);
+  }
+
+  async findOneByWidgetType(
+    workspaceId: string,
+    widgetType: WidgetType,
+  ): Promise<CreateWidgetDto | null> {
+    const widgets = this.getWidgetsMap(workspaceId);
+    const widget = Array.from(widgets.values()).find(
+      (widget) => widget.data.content.widgetType === widgetType,
+    );
+    if (!widget) {
+      return Promise.resolve(null);
     }
     return Promise.resolve(widget);
   }
