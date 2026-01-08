6c8510c4ead345f752a423186f254c34275f5e7e
davidpro08 <davidmoon0817@gmail.com>
2026-01-08 15:45:24 +0900

    refactor: api 요청 보낼 때 오류 문구 지우기

--- frontend/src/common/api/apiClient.ts
+++ frontend/src/common/api/apiClient.ts
@@ -8,7 +8,6 @@ const getApiUrl = () => {
 
 export const apiClient = axios.create({
   baseURL: getApiUrl() + '/api',
-  // 일단 10초로 설정
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
@@ -21,17 +20,6 @@ apiClient.interceptors.response.use(
     return response;
   },
   (error) => {
-    // 에러 처리 로직
-    if (error.response) {
-      // 서버 응답 에러
-      console.error('API Error:', error.response.data);
-    } else if (error.request) {
-      // 요청은 보냈지만 응답을 받지 못함
-      console.error('Network Error:', error.request);
-    } else {
-      // 요청 설정 중 에러
-      console.error('Error:', error.message);
-    }
     return Promise.reject(error);
   },
 );
