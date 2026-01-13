1a8c445e8a8f95fc4620e3d6c617d30e9d81811c
GyuriKim <artgr2062@konkuk.ac.kr>
2026-01-13 17:49:19 +0900

    feat: TechStackWidget에서 기존 Select 컴포넌트를 SelectInput으로 변경

--- frontend/src/features/widgets/techStack/components/techStackWidget/TechStackWidget.tsx
+++ frontend/src/features/widgets/techStack/components/techStackWidget/TechStackWidget.tsx
@@ -3,15 +3,6 @@ import WidgetContainer from '@/common/components/widget/WidgetContainer';
 import WidgetHeader from '@/common/components/widget/WidgetHeader';
 import { LuLayers } from 'react-icons/lu';
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@/common/components/shadcn/select';
 import { useState } from 'react';
 import { Button } from '@/common/components/shadcn/button';
 import { SUBJECT_GROUPS } from '@/common/mocks/techStacks';
@@ -21,6 +12,7 @@ import type { DragEndEvent } from '@dnd-kit/core';
 
 import SelectedTechStackBox from './SelectedTechStackBox';
 import type { TechStack } from '@/features/widgets/techStack/types/techStack';
+import SelectInput from '@/common/components/SelectInput';
 
 function TechStackWidget({ id, position, width, height }: WidgetData) {
   const [isTechStackModalOpen, setIsTechStackModalOpen] = useState(false);
@@ -63,27 +55,10 @@ function TechStackWidget({ id, position, width, height }: WidgetData) {
           icon={<LuLayers className="text-primary" size={18} />}
         />
         <section className="flex flex-col gap-4">
-          <Select>
-            <div className="flex items-center gap-2 font-bold">
-              <div className="shrink-0">주제 :</div>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="논의할 주제를 선택해주세요." />
-              </SelectTrigger>
-            </div>
-
-            <SelectContent>
-              {SUBJECT_GROUPS.map((group) => (
-                <SelectGroup key={group.category}>
-                  <SelectLabel>{group.category}</SelectLabel>
-                  {group.subjects.map((subject) => (
-                    <SelectItem key={subject.value} value={subject.value}>
-                      {subject.label}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              ))}
-            </SelectContent>
-          </Select>
+          <div className="flex items-center gap-2 font-bold">
+            <div className="shrink-0">주제 :</div>
+            <SelectInput initialOptions={SUBJECT_GROUPS} />
+          </div>
 
           <SelectedTechStackBox
             selectedTechStacks={selectedTechStacks}
