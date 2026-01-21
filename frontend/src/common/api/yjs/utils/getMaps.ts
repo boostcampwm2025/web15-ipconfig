38358878d7f53cab33028e18eb297fd155c36a9a
as-zini <aszini@naver.com>
2026-01-21 21:02:11 +0900

    feat: Y.Map 호출 유틸함수 작성

--- frontend/src/common/api/yjs/utils/getMaps.ts
+++ frontend/src/common/api/yjs/utils/getMaps.ts
@@ -1,10 +1,10 @@
 import * as Y from 'yjs';
-import { doc } from './instance';
+import { doc } from '../instance';
 
 export const getRootMap = () => doc.getMap('root');
 
 export const getWidgetsMap = () =>
-  getRootMap().get('widgets') as Y.Map<Y.Map<undefined>>;
+  getRootMap().get('widgets') as Y.Map<Y.Map<unknown>>;
 
 export const getWidgetOrderArray = () =>
   getRootMap().get('widgetOrder') as Y.Array<string>;
