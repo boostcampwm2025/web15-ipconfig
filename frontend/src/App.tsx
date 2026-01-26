e697e438235923cc20aceab85aad405324343298
davidpro08 <davidmoon0817@gmail.com>
2026-01-26 17:28:34 +0900

    chore: 쓸모 없는 경로 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,11 @@
 import { Routes, Route } from 'react-router';
 import WorkSpacePage from './pages/workspace/WorkSpacePage';
-import LandingPage from './pages/workspace/LandingPage';
-import MakeWorkspacePage from './pages/workspace/MakeWorkspacePage';
-import JoinWorkspacePage from './pages/workspace/JoinWorkspacePage';
+import LandingPage from './pages/landingPage/LandingPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/workspace/make" element={<MakeWorkspacePage />} />
-      <Route
-        path="/workspace/join/:workspaceId"
-        element={<JoinWorkspacePage />}
-      />
       <Route path="/workspace/:workspaceId" element={<WorkSpacePage />} />
     </Routes>
   );
