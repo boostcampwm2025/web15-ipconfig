db6b8cbd9500ac8181644027c2fd10570a632437
davidpro08 <davidmoon0817@gmail.com>
2026-01-26 16:12:39 +0900

    feat: 방 만들기 CRUD

---
+++ backend/src/workspace/workspace.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
+import { WorkspaceService } from './workspace.service';
+import { customAlphabet } from 'nanoid';
+
+@Controller('workspace')
+export class WorkspaceController {
+  constructor(private readonly workspaceService: WorkspaceService) {}
+
+  @Get(':workspaceId')
+  getWorkspaceIsExists(@Param('workspaceId') workspaceId: string) {
+    return this.workspaceService.isExistsWorkspace(workspaceId);
+  }
+
+  @Post()
+  createWorkspaceWithRandomId() {
+    let workspaceId = customAlphabet(
+      '0123456789abcdefghijklmnopqrstuvwxyz',
+      6,
+    )();
+    while (this.workspaceService.isExistsWorkspace(workspaceId)) {
+      workspaceId = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz', 6)();
+    }
+
+    this.workspaceService.createWorkspace(workspaceId);
+
+    return { workspaceId };
+  }
+
+  @Post(':workspaceId')
+  createWorkspaceWithId(@Param('workspaceId') workspaceId: string) {
+    if (this.workspaceService.isExistsWorkspace(workspaceId)) {
+      throw new ConflictException(
+        `Workspace with ID '${workspaceId}' already exists`,
+      );
+    }
+
+    this.workspaceService.createWorkspace(workspaceId);
+
+    return { workspaceId };
+  }
+}
