a4859c35a95053a19baf50870648ed5992051667
davidpro08 <davidmoon0817@gmail.com>
2026-01-26 17:14:04 +0900

    feat: 프론트 API 다른 파일로 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router';
 import WorkSpacePage from './pages/workspace/WorkSpacePage';
 import LandingPage from './pages/workspace/LandingPage';
+import MakeWorkspacePage from './pages/workspace/MakeWorkspacePage';
+import JoinWorkspacePage from './pages/workspace/JoinWorkspacePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/workspace/make" element={<MakeWorkspacePage />} />
+      <Route
+        path="/workspace/join/:workspaceId"
+        element={<JoinWorkspacePage />}
+      />
       <Route path="/workspace/:workspaceId" element={<WorkSpacePage />} />
     </Routes>
   );
