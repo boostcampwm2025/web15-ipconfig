059527cce1652d496f711f7ed935494319e202c9
davidpro08 <davidmoon0817@gmail.com>
2026-01-28 23:55:47 +0900

    chore: 삭제된 모듈 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,19 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkspaceModule } from './workspace/workspace.module';
-import { WidgetModule } from './widget/widget.module';
-import { CursorModule } from './cursor/cursor.module';
 import { MarkdownModule } from './markdown/markdown.module';
 import { CollaborationModule } from './collaboration/collaboration.module';
 
 @Module({
-  imports: [
-    WorkspaceModule,
-    WidgetModule,
-    CursorModule,
-    MarkdownModule,
-    CollaborationModule,
-  ],
+  imports: [WorkspaceModule, MarkdownModule, CollaborationModule],
   controllers: [AppController],
   providers: [AppService],
 })
