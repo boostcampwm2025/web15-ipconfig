f20ccfd620509da0313e5c8ae74493d625ffa152
GyuriKim <artgr2062@konkuk.ac.kr>
2026-01-28 18:52:53 +0900

    refactor(workspace): 사용하지 않는 코드와 주석 정리

--- frontend/src/common/hooks/useCollaboration.ts
+++ frontend/src/common/hooks/useCollaboration.ts
@@ -3,23 +3,11 @@ import { connectProvider } from '../api/yjs/instance';
 import { bindYjsToZustand } from '../api/yjs/sync';
 
 export const useCollaboration = (documentName: string) => {
-  // const user = useUserStore((state) => state.user);
-
   useEffect(() => {
     if (!documentName) return;
 
     // 싱글톤 Provider 연결
     connectProvider(documentName);
-
-    // // 유저 정보가 있으면 Awareness에 등록
-    // if (user) {
-    //   setLocalUser({
-    //     id: user.id,
-    //     nickname: user.nickname,
-    //     color: user.color,
-    //     backgroundColor: user.backgroundColor,
-    //   });
-    // }
 
     const cleanupSync = bindYjsToZustand();
 
