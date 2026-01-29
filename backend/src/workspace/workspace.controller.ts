b7484bb9878da641e5d64d0909f552db1ea2881f
GyuriKim <artgr2062@konkuk.ac.kr>
2026-01-29 18:38:49 +0900

    refactor(workspace): 컨트롤러 메서드 서비스 메서드 호출로 리팩토링
    - 불필요한 예외 처리 로직 제거
    - Swagger API 응답 데코레이터 추가
    - 메서드 반환 타입을 WorkSpaceResponse로 통합

--- backend/src/workspace/workspace.controller.ts
+++ backend/src/workspace/workspace.controller.ts
@@ -1,13 +1,7 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  ConflictException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { WorkspaceService } from './workspace.service';
-import { customAlphabet } from 'nanoid';
+import { WorkSpaceResponse } from './dto/workspace-response.dto';
+import { ApiResponse } from '@nestjs/swagger';
 import { WorkspaceIdPipe } from './workspcae.pipe';
 
 @Controller('workspace')
@@ -15,45 +9,36 @@ export class WorkspaceController {
   constructor(private readonly workspaceService: WorkspaceService) {}
 
   @Get('join/:workspaceId')
+  @ApiResponse({
+    status: 200,
+    description: '워크스페이스 입장',
+    type: WorkSpaceResponse,
+  })
   joinWorkspaceById(
     @Param('workspaceId', WorkspaceIdPipe) workspaceId: string,
-  ) {
-    if (!this.workspaceService.isExistsWorkspace(workspaceId)) {
-      throw new NotFoundException(`'${workspaceId}' 는 존재하지 않습니다.`);
-    }
-    return { workspaceId };
+  ): WorkSpaceResponse {
+    return this.workspaceService.joinWorkSpace(workspaceId);
   }
 
   @Post('make')
-  createWorkspaceWithRandomIdMake() {
-    let workspaceId = customAlphabet(
-      '0123456789abcdefghijklmnopqrstuvwxyz',
-      10,
-    )();
-    while (this.workspaceService.isExistsWorkspace(workspaceId)) {
-      workspaceId = customAlphabet(
-        '0123456789abcdefghijklmnopqrstuvwxyz',
-        10,
-      )();
-    }
-
-    this.workspaceService.createWorkspace(workspaceId);
-
-    return { workspaceId };
+  @ApiResponse({
+    status: 200,
+    description: '워크스페이스 생성',
+    type: WorkSpaceResponse,
+  })
+  createWorkspaceWithRandomIdMake(): WorkSpaceResponse {
+    return this.workspaceService.makeWorkspace();
   }
 
   @Post('make/:workspaceId')
+  @ApiResponse({
+    status: 200,
+    description: '지정된 ID로 워크스페이스 생성',
+    type: WorkSpaceResponse,
+  })
   createWorkspaceWithId(
     @Param('workspaceId', WorkspaceIdPipe) workspaceId: string,
   ) {
-    if (this.workspaceService.isExistsWorkspace(workspaceId)) {
-      throw new ConflictException(
-        `이미 '${workspaceId}' 워크스페이스가 존재합니다.`,
-      );
-    }
-
-    this.workspaceService.createWorkspace(workspaceId);
-
-    return { workspaceId };
+    return this.workspaceService.makeWorkspace(workspaceId);
   }
 }
