55e3af7225e155ed7806455afadb9a933123acc6
snailw <lth010618@gmail.com>
2026-01-29 17:50:16 +0900

    refactor: CollaborationModule에 StorageAdapter 등록

--- backend/src/collaboration/collaboration.module.ts
+++ backend/src/collaboration/collaboration.module.ts
@@ -1,10 +1,19 @@
 import { Module, Global } from '@nestjs/common';
 import { CollaborationService } from './collaboration.service';
 import { YjsDocReaderService } from './yjs-doc-reader.service';
+import { StorageAdapter } from './storage/storage.interface';
+import { RedisStorageAdapter } from './storage/redis-storage.adapter';
 
 @Global()
 @Module({
-  providers: [CollaborationService, YjsDocReaderService],
-  exports: [CollaborationService, YjsDocReaderService],
+  providers: [
+    CollaborationService,
+    YjsDocReaderService,
+    {
+      provide: StorageAdapter,
+      useClass: RedisStorageAdapter,
+    },
+  ],
+  exports: [CollaborationService, YjsDocReaderService, StorageAdapter],
 })
 export class CollaborationModule {}
