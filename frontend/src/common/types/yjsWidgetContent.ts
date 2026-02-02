244315f13c166d22ee9343ccadf73736bcf3f803
as-zini <aszini@naver.com>
2026-02-02 18:44:02 +0900

    fix: 타입 오류 해결

--- frontend/src/common/types/yjsWidgetContent.ts
+++ frontend/src/common/types/yjsWidgetContent.ts
@@ -29,7 +29,7 @@ export interface GitConventionContent {
   strategy: Selector;
   branchRules: {
     mainBranch: string;
-    developBranch?: string;
+    developBranch?: string | null;
     prefixes: MultiSelector;
   };
   commitConvention: {
