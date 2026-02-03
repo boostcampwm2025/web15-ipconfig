340bf98bb87081de0a7109dd9c70472f1b7acdc0
snailw <lth010618@gmail.com>
2026-02-03 14:25:05 +0900

    refactor: Collaboration 빌더 클래스 분리

--- backend/src/markdown/builders/index.ts
+++ backend/src/markdown/builders/index.ts
@@ -3,3 +3,4 @@ export * from './git-convention.builder';
 export * from './tech-stack.builder';
 export * from './naming-convention.builder';
 export * from './format.builder';
+export * from './collaboration.builder';
