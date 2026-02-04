2713f9bff2094be61a631525fcb438ad452d56e5
snailw <lth010618@gmail.com>
2026-02-04 21:42:26 +0900

    refactor: workspace 모듈 TTL, ID 생성 매직넘버 상수화

---
+++ backend/src/workspace/constants/workspace.constants.ts
@@ -0,0 +1,6 @@
+export const WORKSPACE_TTL_DAYS = 3;
+export const WORKSPACE_TTL_MS = WORKSPACE_TTL_DAYS * 24 * 60 * 60 * 1000;
+export const WORKSPACE_TTL_SECONDS = WORKSPACE_TTL_DAYS * 24 * 60 * 60;
+
+export const WORKSPACE_ID_ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz';
+export const WORKSPACE_ID_LENGTH = 10;
