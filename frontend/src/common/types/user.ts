42f6a6da52d82b3ebe3212c4b7d353b3cd3bf028
GyuriKim <artgr2062@konkuk.ac.kr>
2026-02-04 20:50:09 +0900

    feat: 커서 정보 타입 추가 및 ManipulationState 수정
    - Cursor 인터페이스를 추가하여 기존의 커서의 위치 및 타임스탬프 정보에 커서 타입, 메시지 정보를 통합
    - User 인터페이스에서 cursor 타입을 Position에서 Cursor로 변경
    - ManipulationState에서 type 필드를 제거하고 커서 타입으로 옮김

--- frontend/src/common/types/user.ts
+++ frontend/src/common/types/user.ts
@@ -1,4 +1,10 @@
-import type { Position } from './canvas';
+interface Cursor {
+  x: number;
+  y: number;
+  type: 'default' | 'chat';
+  message?: string;
+  ts: number;
+}
 
 // 유저가 조작하고 있는 위젯 레이아웃
 export interface ManipulationLayout {
@@ -11,7 +17,6 @@ export interface ManipulationLayout {
 // 유저가 조작하고 있는 위젯 정보
 export interface ManipulationState {
   widgetId: string;
-  type: 'move' | 'resize';
   layout: ManipulationLayout;
 }
 
@@ -19,7 +24,7 @@ export interface User {
   id: string; // 유저 ID
   nickname: string; // 유저 닉네임
   color: string; // 커서 색깔
-  cursor: Position; // 커서 위치
+  cursor: Cursor; // 커서 정보
   // 유저가 조작하고 있는 위젯 정보
   manipulationState: ManipulationState;
 }
