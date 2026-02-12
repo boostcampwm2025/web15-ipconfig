ffebb9b80aa78b51b2e76a65dffdf772e66525d3
GyuriKim <artgr2062@konkuk.ac.kr>
2026-02-12 23:03:19 +0900

    fix(useJoinWorkspace): 워크스페이스 코드 오류 메세지를 토스트로 변경

--- frontend/src/common/hooks/useJoinWorkspace.ts
+++ frontend/src/common/hooks/useJoinWorkspace.ts
@@ -4,6 +4,7 @@ import { workspaceApi } from '@/common/api/workspaceApi';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { joinCodeSchema, type JoinCode } from '@/common/schemas/joinCodeSchema';
+import { toast } from 'sonner';
 
 export function useJoinWorkspace() {
   const navigate = useNavigate();
@@ -11,7 +12,6 @@ export function useJoinWorkspace() {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors },
   } = useForm<JoinCode>({
     resolver: zodResolver(joinCodeSchema),
@@ -26,18 +26,15 @@ export function useJoinWorkspace() {
       if (data.code !== '') {
         const { exists } = await workspaceApi.get(data.code);
         if (exists) navigate(`/workspace/${data.code}`);
-        else
-          setError('code', {
-            message: '존재하지 않는 워크스페이스 코드입니다.',
-          });
+        else toast.error('존재하지 않는 워크스페이스 코드입니다.');
       } else {
-        setError('code', { message: '코드를 입력해주세요.' });
+        toast.error('코드를 입력해주세요.');
       }
     } catch (err) {
       if (err instanceof AxiosError && err.response?.status === 404) {
-        setError('code', { message: '존재하지 않는 워크스페이스 코드입니다.' });
+        toast.error('존재하지 않는 워크스페이스 코드입니다.');
       } else {
-        setError('code', { message: '워크스페이스 참가에 실패했습니다.' });
+        toast.error('워크스페이스 참가에 실패했습니다.');
       }
     }
   };
